767846c11f80518a88e08ff44eb54824d3d8e1ef
leonardo gomes <leonardo.gomes@lit.ifce.edu.br>
2020-04-13 15:40:14 -0300

    Initial Commit

---
+++ SeeImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeImages
+//
+//  Created by Leonardo Gomes on 13/04/20.
+//  Copyright © 2020 Leonardo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

6a963963ec76c24d1f02db22b1a3d1fb7b60ff74
leonardo gomes <leonardo.gomes@lit.ifce.edu.br>
2020-04-15 11:37:04 -0300

    First commit, uploading all prject

--- SeeImages/ViewController.swift
+++ SeeImages/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UITableViewController {
+
+    var pictures: [String] = []
+//    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Galaxy Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        if let path = Bundle.main.resourcePath {
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+            for item in items {
+                if item.hasPrefix("img") {
+                    pictures.append(item)
+                }
+            }
+            
+            
+        }
+        
+        print(pictures)
+        print(pictures[1] > pictures[2])
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
